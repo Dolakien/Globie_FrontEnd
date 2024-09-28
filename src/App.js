b4bc185a29258434936c8f90dc99da7bccb27dfe
Huỳnh Văn Tường <144124462+Tuongvxvvvvsvs@users.noreply.github.com>
2024-09-28 08:35:20 +0700

    add layout client

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-// src/App.js
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-  useLocation,
-} from "react-router-dom";
-import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login/Login";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+import ClientLayout from "./layouts/ClientLayout/ClientLayout";
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
-        <HeaderWithConditionalDisplay />
-        <Routes>
-          {/* Redirect from root path to /home */}
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route element={<ClientLayout />}>
+          <Route element={<Home />} path="/" />
+        </Route>
+
+        {/* auth route */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
     </Router>
   );
-};
-
-// Component to conditionally display Header based on current path
-const HeaderWithConditionalDisplay = () => {
-  const location = useLocation();
-  const isShowHeader = !["/login", "/forgot-password"].includes(
-    location.pathname
-  );
-  return isShowHeader ? <Header /> : null;
 };
 
 export default App;
